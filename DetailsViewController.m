6d6ddc856d9f29b7747df1e7bcb588716583c64a
Ernst Haagsman <eh@ernsts-mac.local>
2015-11-10 17:08:49 -0800

    Find distance without error handling

--- DetailsViewController.m
+++ DetailsViewController.m
@@ -8,7 +8,9 @@
 
 #import "DetailsViewController.h"
 
-@interface DetailsViewController ()
+@interface DetailsViewController (){
+    LABBuilding* _building;
+}
 
 @end
 
@@ -73,13 +75,40 @@
     [self.addressLabel sizeToFit];
     self.distanceLabel.text = @"Loading";
     [self.buildingImageName setImage: [UIImage imageNamed: building.imageName]];
+    _building = building;
 }
 
 #pragma mark Location Delegate
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
 {
-    CLLocation *loc = locations.lastObject;
+    CLLocationCoordinate2D loc = locations.lastObject.coordinate;
+    CLLocationCoordinate2D dest = _building.latlon.coordinate;
+    
+    NSMutableString *URLstring = [[NSMutableString alloc] initWithCapacity:300];
+    [URLstring appendString: @"https://maps.googleapis.com/maps/api/distancematrix/json?"];
+    [URLstring appendString: [NSString stringWithFormat:@"origins=%f,%f&", loc.latitude, loc.longitude]];
+    [URLstring appendString: [NSString stringWithFormat:@"destinations=%f,%f&", dest.latitude, dest.longitude]];
+    [URLstring appendString: @"mode=walking&"];
+    [URLstring appendString: @"key=AIzaSyDefwwfJrWKTXqbQog7DjsBVs8tFArHva4&"];
+    
+    NSURL *requestURL = [NSURL URLWithString: URLstring];
+    NSURLRequest *request = [NSURLRequest requestWithURL: requestURL];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                 options:0
+                                                                   error:NULL];
+        
+        // Get the important bits from the JSON structure returned by google
+        NSArray *rows = [json objectForKey:@"rows"];
+        NSDictionary *row = [rows objectAtIndex:0];
+        NSArray *elements = [row objectForKey:@"elements"];
+        NSDictionary *results = [elements objectAtIndex:0];
+        NSDictionary *distance = [results objectForKey:@"distance"];
+        NSDictionary *duration = [results objectForKey:@"duration"];
+        
+        self.distanceLabel.text = [NSString stringWithFormat: @"%@ / %@ walk", [distance objectForKey:@"text"], [duration objectForKey:@"text"]];
+    }];
 }
 
 
